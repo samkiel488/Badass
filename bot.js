d7e01d1c6db6c1ebcca64b78f284f8d3e4a4a674
Ԇ・SAMKIEL <samuelezekiel488@gmail.com>
2025-05-08 21:17:14 +0100

    Changed to SAMKIELAI

--- bot.js
+++ bot.js
@@ -1,41 +1,48 @@
 // Pair code link > https://qr-hazel-alpha.vercel.app/md
 // Get your session id and put it in the space created for it don't remove "
-const { spawnSync } = require('child_process')
-const { existsSync, writeFileSync } = require('fs')
+const { spawnSync } = require('child_process');
+const { existsSync, writeFileSync } = require('fs');
 
-const SESSION_ID = 'levanter_SAMKIEL' // Edit this line only, don't remove ' <- this symbol
+const SESSION_ID = 'levanter_931dd041d8bcb46b198e7d733ad608241'; // Edit this line only, don't remove ' <- this symbol
 
-if (!existsSync('levanter')) {
-  console.log('Cloning the repository...')
+if (!existsSync('samkielai')) {
+  console.log('Cloning the repository...');
   const cloneResult = spawnSync(
     'git',
-    ['clone', 'https://github.com/lyfe00011/levanter.git', 'levanter'],
+    ['clone', 'https://github.com/samkiel488/SAMKIELAI.git', 'samkielai'],
     {
       stdio: 'inherit',
     }
-  )
-
+  );
+  
   if (cloneResult.error) {
-    throw new Error(`Failed to clone the repository: ${cloneResult.error.message}`)
+    throw new Error(`Failed to clone the repository: ${cloneResult.error.message}`);
   }
-
-  const configPath = 'levanter/config.env'
+  
+  const configPath = 'samkielai/config.env';
   try {
-    console.log('Writing to config.env...')
-    writeFileSync(configPath, `VPS=true\nSESSION_ID=${SESSION_ID}\nSUDO=2348087357158, 2347025067494\nTZ=Africa/Lagos\nSTICKER_PACKNAME=Ԇ・SAMKIEL`)
+    console.log('Writing to config.env...');
+    writeFileSync(
+      configPath,
+      `VPS=true\n` +
+      `SESSION_ID=${SESSION_ID}\n` +
+      `SUDO=2348087357158, 2347025067494\n` +
+      `TZ=Africa/Lagos\n` +
+      `STICKER_PACKNAME=Ԇ・SAMKIEL\n`
+    );
   } catch (err) {
-    throw new Error(`Failed to write to config.env: ${err.message}`)
+    throw new Error(`Failed to write to config.env: ${err.message}`);
   }
-
-  console.log('Installing dependencies...')
+  
+  console.log('Installing dependencies...');
   const installResult = spawnSync('yarn', ['install', '--network-concurrency', '3'], {
-    cwd: 'levanter',
+    cwd: 'samkielai',
     stdio: 'inherit',
-  })
-
+  });
+  
   if (installResult.error) {
-    throw new Error(`Failed to install dependencies: ${installResult.error.message}`)
+    throw new Error(`Failed to install dependencies: ${installResult.error.message}`);
   }
 }
 
-spawnSync('yarn', ['start'], { cwd: 'levanter', stdio: 'inherit' })
+spawnSync('yarn', ['start'], { cwd: 'samkielai', stdio: 'inherit' });
